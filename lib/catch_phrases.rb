73d83e036be96f05b14003c3ba073a0dc0bdba1d
charming-input-2976 <mbingramm@gmail.com>
2020-11-19 22:34:49 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,25 @@
+
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  status
+end
+
+def link
+  says = "It's Dangerous To Go Alone! Take This."
+  puts says
+  says
+end
+
+def all_phrases()
+  declare = "#{mario} #{toadstool} #{link}"
+  puts declare
+  declare
 end
